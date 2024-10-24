51475ce44fd26ce90262b535704ad75d2f399a0f
Raphaël <barre.raphael97@gmail.com>
2024-10-24 16:27:33 +0200

    ReactBasic5

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,19 +1,23 @@
 
 
-  function PokemonCard({ pokemon }) {
-    // Affichage de l'objet props dans la console du navigateur
-    
-  
-    return (
-      <figure>
-        {pokemon.imgSrc ? (
-          <img src={pokemon.imgSrc} alt={pokemon.name} />
-        ) : (
-          <p>???</p> // Si l'image n'existe pas, afficher "???"
-        )}
-        <figcaption>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</figcaption>
-      </figure>
-    );
-  }
-  
-  export default PokemonCard;  
+  // Définition de l'interface Pokemon
+interface Pokemon {
+  name: string;       // name est obligatoire
+  imgSrc?: string;    // imgSrc est optionnel
+}
+
+// Typage des props dans le composant PokemonCard
+function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
+  return (
+    <figure>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p> // Si l'image n'existe pas, afficher "???"
+      )}
+      <figcaption>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</figcaption>
+    </figure>
+  );
+}
+
+export default PokemonCard;  
